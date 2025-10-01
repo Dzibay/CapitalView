2a08e69161773d22f23ee4039c2aad02ac2b094f
Dzibay <113441228+Dzibay@users.noreply.github.com>
2025-10-02 01:07:42 +0300

    View user portfolio from tinkoff invest tocken

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app, origins=["http://localhost:5173"])
+    
     # импортируем и регистрируем роуты
     from .routes import portfolio_bp
     app.register_blueprint(portfolio_bp)
