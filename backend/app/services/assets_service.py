b982b37749e5e4450df3ae7c5a02bf4045c29d8d
Dzibay <113441228+Dzibay@users.noreply.github.com>
2026-01-18 21:42:05 +0300

    Debug portfolio_asset dividend list

--- backend/app/services/assets_service.py
+++ backend/app/services/assets_service.py
@@ -308,6 +308,7 @@ def get_portfolio_asset_info(portfolio_asset_id: int):
         
         portfolio_asset = asset_meta[0]
         asset_id = portfolio_asset.get("asset_id")
+        portfolio_id = portfolio_asset.get("portfolio_id")
         
         # Получаем транзакции по этому активу
         transactions = table_select(
@@ -327,7 +328,7 @@ def get_portfolio_asset_info(portfolio_asset_id: int):
             payout_operations = table_select(
                 "cash_operations",
                 select="*",
-                filters={"asset_id": asset_id},
+                filters={"portfolio_id": portfolio_id, "asset_id": asset_id},
                 in_filters={"type": [3, 4]},  # 3 = дивиденды, 4 = купоны
                 order={"column": "date", "desc": True}
             )
