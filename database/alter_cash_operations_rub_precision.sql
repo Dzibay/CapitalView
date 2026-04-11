85729040c2c0c7778af9023c23e7e496e62fdd59
Dzibay <113441228+Dzibay@users.noreply.github.com>
2026-04-12 02:54:43 +0300

    Remove migration sql scripts

--- database/alter_cash_operations_rub_precision.sql
+++
@@ -1,5 +0,0 @@
--- Увеличить точность рублёвых сумм в cash_operations (накопление баланса и сверка с брокером).
--- Выполнить один раз на существующей БД после бэкапа.
-ALTER TABLE public.cash_operations
-  ALTER COLUMN amount_rub TYPE numeric(20,6),
-  ALTER COLUMN commission_rub TYPE numeric(20,6);
